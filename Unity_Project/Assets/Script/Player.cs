3222eb6b12996d3922677d51787704be647a3b67
gysju <gysenjulien@gmail.com>
2016-09-29 13:38:13 +0200

    now you can easily switch between two movement types

--- Unity_Project/Assets/Script/Player.cs
+++ Unity_Project/Assets/Script/Player.cs
@@ -25,17 +25,23 @@ public class Player : Unit {
 	
 	void Update ()
     {
-        switch (moveSystem)
-        {
-            case MoveSystem.MoveSystem_type1:
-                PointingSystem();
-                break;
-			case MoveSystem.MoveSystem_type2:
-				orientationSystem ();
-                break;
-            case MoveSystem.MoveSystem_type3:
-                break;
-        }
+
+		foreach(SixenseHand hand in hands)
+		{
+			if (hand.m_controller != null) 
+			{
+				if(hand.m_controller.GetButton(SixenseButtons.ONE))
+				{
+					PauseNavMesh ();
+					orientationSystem (hand);
+					return;
+				}
+				else
+				{
+					PointingSystem (hand);
+				}
+			}
+		}
     }
 
 	void Input()
@@ -43,71 +49,62 @@ public class Player : Unit {
 
 	}
 
-	void orientationSystem()
+	void orientationSystem( SixenseHand hand)
 	{
 		Vector3 dir = Vector3.zero;
-		foreach (SixenseHand hand in hands)
-		{
-			if(hand.m_controller != null)
-			{
-				float x = hand.transform.rotation.x;
-				float y = hand.transform.rotation.y;
-				Debug.Log (hands [0].transform.rotation);
 
-				const float NEUTRAL_Z = -0.3f;
-				const float NEUTRAL_X = 0f;
+		float x = hand.transform.rotation.x;
+		float y = hand.transform.rotation.y;
+		Debug.Log (hands [0].transform.rotation);
 
-				const float MAX_FORWARD = 0f;
-				const float MIN_FORWARD = -0.2f;
-				const float MAX_BACKWARD = -0.6f;
-				const float MIN_BACKWARD = -0.4f;
-				const float MAX_LEFT = -0.2f;
-				const float MIN_LEFT = -0.1f;
-				const float MAX_RIGHT = 0.2f;
-				const float MIN_RIGHT = 0.1f;
+		const float NEUTRAL_Z = -0.3f;
+		const float NEUTRAL_X = 0f;
 
-				float zdir = 0;
-				if (x > NEUTRAL_Z) //avant
-					zdir = (Mathf.InverseLerp (MIN_FORWARD, MAX_FORWARD, x) * Time.deltaTime);
-				if (x < NEUTRAL_Z) //arrière
-					zdir = -(Mathf.InverseLerp (MIN_BACKWARD, MAX_BACKWARD, x) * Time.deltaTime);
-		
-				float xdir = 0;
-				if (y < NEUTRAL_X) //gauche
-					xdir = -(Mathf.InverseLerp (MIN_LEFT, MAX_LEFT, y) * Time.deltaTime);
-				if (y > NEUTRAL_X) //droite
-					xdir = (Mathf.InverseLerp (MIN_RIGHT, MAX_RIGHT, y) * Time.deltaTime);
+		const float MAX_FORWARD = 0f;
+		const float MIN_FORWARD = -0.2f;
+		const float MAX_BACKWARD = -0.6f;
+		const float MIN_BACKWARD = -0.4f;
+		const float MAX_LEFT = -0.2f;
+		const float MIN_LEFT = -0.1f;
+		const float MAX_RIGHT = 0.2f;
+		const float MIN_RIGHT = 0.1f;
 
-				dir += new Vector3 (xdir, 0f, zdir);
-			}
-		}
+		float zdir = 0;
+		if (x > NEUTRAL_Z) //avant
+			zdir = (Mathf.InverseLerp (MIN_FORWARD, MAX_FORWARD, x) * Time.deltaTime);
+		if (x < NEUTRAL_Z) //arrière
+			zdir = -(Mathf.InverseLerp (MIN_BACKWARD, MAX_BACKWARD, x) * Time.deltaTime);
+
+		float xdir = 0;
+		if (y < NEUTRAL_X) //gauche
+			xdir = -(Mathf.InverseLerp (MIN_LEFT, MAX_LEFT, y) * Time.deltaTime);
+		if (y > NEUTRAL_X) //droite
+			xdir = (Mathf.InverseLerp (MIN_RIGHT, MAX_RIGHT, y) * Time.deltaTime);
+
+			dir += new Vector3 (xdir, 0f, zdir);
+
 		transform.Translate (dir);
 	}
 
-    void PointingSystem()
+	void PointingSystem(SixenseHand hand)
     {
-		foreach (SixenseHand hand in hands)
+		if (hand.m_controller.GetButton(SixenseButtons.BUMPER))
 		{
-			if (hand.m_controller != null && hand.m_controller.GetButton(SixenseButtons.BUMPER))
+			RaycastHit hit;
+			if (Physics.Raycast(hand.transform.position, hand.transform.forward, out hit))
 			{
-				RaycastHit hit;
-				if (Physics.Raycast(hand.transform.position, hand.transform.forward, out hit))
-				{
-					LineRenderer line = hand.GetComponent<LineRenderer> ();
-					line.SetPosition (0, transform.position);
-					line.SetPosition (1, hit.point);
-					destination = hit.point;
-				}
+				LineRenderer line = hand.GetComponent<LineRenderer> ();
+				line.SetPosition (0, transform.position);
+				line.SetPosition (1, hit.point);
+				destination = hit.point;
 			}
 		}
-        foreach (SixenseHand hand in hands)
+
+		if (destination != Vector3.zero && hand.m_controller.GetButtonUp(SixenseButtons.BUMPER))
         {
-			if (hand.m_controller != null && destination != Vector3.zero && hand.m_controller.GetButtonUp(SixenseButtons.BUMPER))
-            {
-				navMeshAgent.Resume ();
-				setDestination(destination);
-				destination = Vector3.zero;
-            }
-        }
+			navMeshAgent.Resume ();
+			setDestination(destination);
+			destination = Vector3.zero;
+        } 
     }
 }
