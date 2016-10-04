eeb1210f3cb6709f682a3c2b8622c4c2d165cc6b
gysju <gysenjulien@gmail.com>
2016-10-04 09:38:27 +0200

    tweek some values

--- Unity_Project/Assets/script/Units/Player.cs
+++ Unity_Project/Assets/script/Units/Player.cs
@@ -47,14 +47,14 @@ public class Player : Unit
 	void shoot()
 	{
 		if (hands [0].m_controller != null && hands [1].m_controller != null) {
-			if (Input.GetMouseButtonDown (0) || hands [1].m_controller.GetButtonDown (SixenseButtons.TRIGGER))
+			if (Input.GetMouseButtonDown (0) || hands [0].m_controller.GetButtonDown (SixenseButtons.TRIGGER))
 				m_leftWeapon.TriggerPressed ();
-			if (Input.GetMouseButtonDown (1) || hands [0].m_controller.GetButtonDown (SixenseButtons.TRIGGER))
+			if (Input.GetMouseButtonDown (1) || hands [1].m_controller.GetButtonDown (SixenseButtons.TRIGGER))
 				m_rightWeapon.TriggerPressed ();
 			
-			if (Input.GetMouseButtonUp (0) || hands [1].m_controller.GetButtonUp (SixenseButtons.TRIGGER))
+			if (Input.GetMouseButtonUp (0) || hands [0].m_controller.GetButtonUp (SixenseButtons.TRIGGER))
 				m_leftWeapon.TriggerReleased ();
-			if (Input.GetMouseButtonUp (1) || hands [0].m_controller.GetButtonUp (SixenseButtons.TRIGGER))
+			if (Input.GetMouseButtonUp (1) || hands [1].m_controller.GetButtonUp (SixenseButtons.TRIGGER))
 				m_rightWeapon.TriggerReleased ();
 		} 
 		else 
