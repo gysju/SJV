f2dc462919a5f2f6faec3e04e5335d9ec0d02ced
gysju <gysenjulien@gmail.com>
2016-11-28 12:54:05 +0100

    now you can select with psmove the buttons on menus

--- Unity_Project/Assets/Script/Weapon/LaserPointingSystem.cs
+++ Unity_Project/Assets/Script/Weapon/LaserPointingSystem.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class LaserPointingSystem : MonoBehaviour {
@@ -10,12 +12,17 @@ public class LaserPointingSystem : MonoBehaviour {
 
 	private LineRenderer lineRenderer;
 	private RaycastHit hit;
+
+	private EventSystem eventSystem;
+
 #if UNITY_PS4
 	private MoveController move;
 #endif
+	[SerializeField]
     private int count = 1;
 	void Start () 
 	{
+		eventSystem =  GameObject.Find("EventSystem").GetComponent<EventSystem>();
 		lineRenderer = GetComponent<LineRenderer> ();
 #if UNITY_PS4
 		move = GetComponent<MoveController> ();
@@ -26,13 +33,26 @@ public class LaserPointingSystem : MonoBehaviour {
 	{
 		if (count >= 1) {
 			if (Physics.Raycast (transform.position, transform.forward, out hit, 1000.0f, mask)) {
-				lineRenderer.SetPosition (1, Vector3.forward * hit.distance);
+					lineRenderer.SetPosition (1, Vector3.forward * hit.distance);
+
+				Button but = hit.transform.GetComponent<Button>() ;
+				if (but != null) 
+				{
+					but.Select();	
+				}
 #if UNITY_PS4
 				if (move != null)
+				{
 					move.lookAtHit = hit.point;
+				}
 #endif
-			} else {
+			} 
+			else 
+			{
 				lineRenderer.SetPosition (1, Vector3.forward * MinimalDistance);
+
+				//if ( eventSystem != null )
+				//	eventSystem.SetSelectedGameObject(null);
 #if UNITY_PS4
 				if (move != null)
 					move.lookAtHit = transform.position + transform.forward * 1000.0f;
