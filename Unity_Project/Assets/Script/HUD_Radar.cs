777a5bb1d4596eccd79bc00b4610719597cc553b
gysen julien <gysenjulien@gmail.com>
2017-01-28 22:33:51 +0100

    continue to work on the radar

--- Unity_Project/Assets/Script/HUD_Radar.cs
+++ Unity_Project/Assets/Script/HUD_Radar.cs
@@ -4,18 +4,90 @@ using UnityEngine;
 
 public class HUD_Radar : MonoBehaviour {
 
+	public Transform PivotTransfrom;
+	public float Radius = 5.0f;
+	public GameObject UIPrefab;
+
 	private Transform Mecha;
-	private Transform ThisTransform;
+	private Transform infoParent;
+
+	struct Info
+	{
+		public GameObject Target;
+		public GameObject UI;
+	};
+
+	private List<Info> Infos = new List<Info>();
 
 	void Start () 
 	{
 		Mecha = GetComponentInParent<BaseMecha> ().transform;	
-		ThisTransform = transform;
+		GetComponent<SphereCollider>().radius = Radius;
+		infoParent = PivotTransfrom.FindChild ("InfoParent");
 	}
 	
 	void Update () {
 		if (Mecha != null)
-			ThisTransform.localRotation = Quaternion.Euler ( new Vector3 (0.0f, 0.0f, Mecha.localRotation.eulerAngles.y ));
+			PivotTransfrom.localRotation = Quaternion.Euler ( new Vector3 (PivotTransfrom.localRotation.eulerAngles.x, PivotTransfrom.localRotation.eulerAngles.y, Mecha.localRotation.eulerAngles.y ));
+
+		DisplayUnit ();
+	}
+
+	void DisplayUnit()
+	{
+		
+	}
+
+	void OnTriggerEnter( Collider col )
+	{
+		SetInfo (col.gameObject);
+		Debug.Log ("UI Instantiate : " +col.name);
+	}
+		
+	void OnTriggerExit( Collider col )
+	{
+		RemoveInfo (col.gameObject);
+		Debug.Log ("UI destroy : " + col.name);
+	}
+
+	void SetInfo( GameObject obj )
+	{
+		Info info = new Info ();
+
+		info.UI = Instantiate (UIPrefab);
+		info.Target = obj;
+
+		info.UI.transform.parent = infoParent;
+		info.UI.transform.localPosition = Vector3.zero;
+		info.UI.transform.localRotation = Quaternion.identity;
+
+		if (obj.GetComponent<GroundEnemy>() != null) 
+		{
+			info.UI.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", (Texture)Resources.Load ("Radar/Sprite_Tank"));
+		} 
+		else if (obj.GetComponent<AirEnemy>() != null ) 
+		{
+			info.UI.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", (Texture)Resources.Load ("Radar/Sprite_Drone"));
+		} 
+		else 
+		{
+			info.UI.GetComponent<MeshRenderer> ().material.SetTexture ("_MainTex", (Texture)Resources.Load ("Radar/Sprite_Ennemy"));
+		}
+
+		Infos.Add( info );
+	}
+
+	void RemoveInfo(GameObject obj)
+	{
+		foreach(Info info in Infos)
+		{
+			if (info.Target == obj) 
+			{
+				Infos.Remove (info);
+				Destroy (info.UI);
+				break;
+			}
+		}
 	}
 }
  
