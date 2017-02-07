37feb0fc162bfe1b34b90ec3baba34b32149514a
GYSEN Julien <julien.gysen@tech-info.heaj.edu>
2017-02-07 13:33:58 +0100

    add script for display the fps

---
+++ Unity_Project/Assets/Script/Debug/DebugFPS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugFPS : MonoBehaviour {
+
+	private Text text;
+	private int value = 0;
+	private float time = 0;
+
+	void Start () {
+		text = GetComponent<Text> ();	
+	}
+	
+	void Update () {
+
+		if (time >= 1.0f) 
+		{
+			text.text = "fps : " + value;
+			time = 0.0f;
+			value = 0;
+		} 
+		else 
+		{
+			time += Time.deltaTime;
+			value += 1;
+		}
+
+	}
+}
