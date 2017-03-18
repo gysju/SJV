af65eed6ed2f266403aae018369e675b982a3dda
GYSEN Julien <julien.gysen@tech-info.heaj.edu>
2017-03-18 15:37:55 +0100

    fix span enemies

--- Unity_Project/Assets/Script/Units/Enemy/EnemiesManager.cs
+++ Unity_Project/Assets/Script/Units/Enemy/EnemiesManager.cs
@@ -49,7 +49,6 @@ public class EnemiesManager : MonoBehaviour
     public GroundEnemy GetUnusedGroundEnemy(Transform parent)
     {
         GroundEnemy groundEnemy = m_groundPool[0];
-        groundEnemy.gameObject.SetActive(true);
         m_groundPool.RemoveAt(0);
         groundEnemy.transform.parent = parent;
         return groundEnemy;
@@ -58,7 +57,6 @@ public class EnemiesManager : MonoBehaviour
     public AirEnemy GetUnusedAirEnemy(Transform parent)
     {
         AirEnemy airEnemy = m_airPool[0];
-        airEnemy.gameObject.SetActive(true);
         m_airPool.RemoveAt(0);
         airEnemy.transform.parent = parent;
         return airEnemy;
@@ -67,7 +65,6 @@ public class EnemiesManager : MonoBehaviour
     public MobileMineEnemy GetUnusedMobileMine(Transform parent)
     {
         MobileMineEnemy mobileMineEnemy = m_minePool[0];
-        mobileMineEnemy.gameObject.SetActive(true);
         m_minePool.RemoveAt(0);
         mobileMineEnemy.transform.parent = parent;
         return mobileMineEnemy;
@@ -80,17 +77,14 @@ public class EnemiesManager : MonoBehaviour
         if (enemyToPool is AirEnemy)
         {
             m_airPool.Add((AirEnemy)enemyToPool);
-            enemyToPool.gameObject.SetActive(false);
         }
         else if (enemyToPool is MobileMineEnemy)
         {
             m_minePool.Add((MobileMineEnemy)enemyToPool);
-            enemyToPool.gameObject.SetActive(false);
         }
         else if (enemyToPool is GroundEnemy)
         {
             m_groundPool.Add((GroundEnemy)enemyToPool);
-            enemyToPool.gameObject.SetActive(false);
         }
         else
         {
