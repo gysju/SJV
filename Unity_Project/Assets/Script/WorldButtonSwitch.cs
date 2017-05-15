895c859f6f635568a3d1a77f9caf567bca2daf0b
Egon Rivera Fernandez <egon.rivera@gmail.com>
2017-05-15 20:38:21 +0200

    button disable

---
+++ Unity_Project/Assets/Script/WorldButtonSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldButtonSwitch : MonoBehaviour
+{
+    Button button;
+    BoxCollider boxCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button.IsInteractable())
+        {
+            boxCollider.enabled = true;
+        }
+        else
+        {
+            boxCollider.enabled = false;
+        }
+
+    }
+}
