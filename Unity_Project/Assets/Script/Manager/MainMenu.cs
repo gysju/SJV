6f420955e0e920659fa059f13ce3cd08c636ac3a
GYSEN Julien <julien.gysen@tech-info.heaj.edu>
2017-05-18 11:59:34 +0200

    fix main menu

--- Unity_Project/Assets/Script/Manager/MainMenu.cs
+++ Unity_Project/Assets/Script/Manager/MainMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MainMenu : MenuManager
 {
     public CanvasGroup m_title;
+    public CanvasGroup m_levelSelecter;
     public CanvasGroup m_optionMenu;
     public CanvasGroup m_credits;
 
@@ -22,12 +23,26 @@ public class MainMenu : MenuManager
     #region Main Menu
     public void StartButton()
     {
+        HideMenu(m_title);
+        ShowMenu(m_levelSelecter);
+        HideMenu(m_optionMenu);
+        HideMenu(m_credits);
+    }
+
+    public void Button01()
+    {
         LoadLevel(2);
+    }
+
+    public void Button02()
+    {
+        LoadLevel(3);
     }
 
     public void OptionsButton()
     {
         HideMenu(m_title);
+        HideMenu(m_levelSelecter);
         ShowMenu(m_optionMenu);
         HideMenu(m_credits);
     }
